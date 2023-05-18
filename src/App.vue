b94177fc3b6b9a5484a4796048e81039f15cee8c
Marco Giammari <marcogiammari@gmail.com>
2023-05-18 17:53:22 +0200

    import bootstrap

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import AppFooter from './components/AppFooter.vue';
     <AppJumbo />
   </header>
 
-  <main>
+  <main class="fs-5">
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro possimus commodi, aut est aliquid saepe sequi minima cumque unde temporibus impedit obcaecati doloremque soluta fugiat delectus non maxime rem magni.
     Vero quaerat voluptates odio perspiciatis illo assumenda natus reiciendis pariatur consequuntur mollitia veniam, dicta enim quod. Est veritatis maiores nihil, illum excepturi officiis natus. Praesentium fugiat quaerat incidunt nostrum aliquid!
     Quam qui odit facere sint ullam dolorem quod ad labore deleniti quae ratione quo vel praesentium voluptate minima impedit, totam ducimus pariatur non consequuntur accusantium incidunt perferendis nihil! Suscipit, ipsum!
@@ -28,12 +28,11 @@ import AppFooter from './components/AppFooter.vue';
   header {
     display: flex;
     flex-direction: column;
-    gap: 5rem;
+    gap: 3rem;
   }
 
   main {
     width: 70%;
-    font-size: 2rem;
     margin: 0 auto;
   }
 </style>
